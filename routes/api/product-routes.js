04633df86afd4252214500795623b7cc02795b87
Jennifer Arboleda <arboleda.jen@gmail.com>
2023-02-13 11:02:13 -0600

    POST route for Products is now working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -37,16 +37,26 @@ router.get('/:id', async (req, res) => {
 
 
 // CREATE a new product
-router.post('/', (req, res) => { 
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+router.post('/', async (req, res) => { 
+    // try {
+    //   const productData = await Product.create({
+    //    product_name: req.body.product_name,
+    //    price: req.body.price,
+    //    stock: req.body.stock,
+    //    tagIds: [1, 2, 3, 4]
+    //   });
+    //   // res.status(200).json(productData);
+    // } catch (err) {
+    //   res.status(400).json({ message: err.message });
+    //   console.error(err)
+    // }
+
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    tagIds: []
+   })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
