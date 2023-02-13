576425aa35b7933c2af172c0143c8c5e7918e553
Jennifer Arboleda <arboleda.jen@gmail.com>
2023-02-13 12:54:12 -0600

    all DELETE routes are working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
      // be sure to include its associated Category and Tag Data
-      include: [{model: Product}, {model: Tag}]
+      include: [{model: Category}, {model: Tag}]
     });
 
     if (!productData) {
@@ -66,10 +66,17 @@ router.post('/', async (req, res) => {
 });
 
 
-// update product
+// UPDATE product
 router.put('/:id', (req, res) => {
   // update product data
-  Product.update(req.body, {
+  Product.update(
+    {
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      tagIds: []
+     },
+     {
     where: {
       id: req.params.id,
     },
@@ -108,8 +115,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
-});
 
+// DELETE a product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
